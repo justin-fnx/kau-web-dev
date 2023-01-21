14e682bc5894e4785605b47a79e9171db2347b8d
Justin <justin.prod@kakaoent.com>
2023-01-21 14:26:27 +0900

    초기 데이터 세팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name) VALUES (1, '아메리카노');
+INSERT INTO product (id, name) VALUES (2, '카페라떼');
+INSERT INTO product (id, name) VALUES (3, '자몽에이드');
+INSERT INTO product (id, name) VALUES (4, '청포도에이드');
+INSERT INTO product (id, name) VALUES (5, '당근케이크');
